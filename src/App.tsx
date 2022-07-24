9624de10d70fc54c0b2258f5d39392ba9373548b
Edson Candido <edson.cursoprogramacao@gmail.com>
2022-07-24 19:58:25 -0300

    Teste Heroku

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import SearchUser from "./pages/SearchUser/SearchUser";
 
-import "./App.css";
-
 export default function App() {
   return (
     <Router>
